9170f27b40671b16206cc4de762e3df9b1f73dae
azarashi2931 <kousuke.urakawa@gmail.com>
2019-03-20 09:30:57 +0900

    手版の表示UI用スクリプトを移動して修正

---
+++ Assets/Scripts/CerkeOnline/Presentation/Presenter/CurrentTurnPresenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using Azarashi.CerkeOnline.Domain.Entities;
+using Azarashi.CerkeOnline.Application;
+
+namespace Azarashi.CerkeOnline.Presentation.Presenter
+{
+    public class CurrentTurnPresenter : MonoBehaviour
+    {
+        [SerializeField] Text currentTurnText = default;
+
+        void Start()
+        {
+            IGame game = GameController.Instance.Game;
+            this.UpdateAsObservable().TakeUntilDestroy(this)
+                .Select(_ => game.CurrentTurn).DistinctUntilChanged().Subscribe(value =>
+                {
+                    currentTurnText.text = "現在のターン : " + value.ToString();
+                });
+        }
+    }
+}
