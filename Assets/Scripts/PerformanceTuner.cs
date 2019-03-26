607d0d82949e2164a492f118ab52f3a055efa8e8
azarashi2931 <kousuke.urakawa@gmail.com>
2019-03-26 22:21:21 +0900

    パフォーマンスチューニング用のスクリプト.
    作ったけどPlayerSettingsからやるべきかも

---
+++ Assets/Scripts/PerformanceTuner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceTuner : MonoBehaviour
+{
+    [SerializeField] int targetFrameRate = 30;
+    [SerializeField] bool isVSyncActive = true;
+
+    void Start()
+    {
+        Application.targetFrameRate = targetFrameRate;
+        QualitySettings.vSyncCount = isVSyncActive ? 1 : 0;
+    }
+}
