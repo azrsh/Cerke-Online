4f42b0e4731b28691dc99eec9d921375276eb745
azarashi2931 <kousuke.urakawa@gmail.com>
2019-03-29 05:27:27 +0900

    メニューを閉じる機能を追加

---
+++ Assets/Scripts/CerkeOnline/Presentation/View/UI/PauseMenuCloseButtonView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UniRx;
+using Azarashi.CerkeOnline.Application;
+
+public class PauseMenuCloseButtonView : MonoBehaviour
+{
+    [SerializeField] Button closeButton = default;
+
+    void Start()
+    {
+        closeButton.OnClickAsObservable().TakeUntilDestroy(this).Subscribe(_ => SceneManager.UnloadSceneAsync(SceneName.MainSceneUI.PauseMenu));
+    }
+}
