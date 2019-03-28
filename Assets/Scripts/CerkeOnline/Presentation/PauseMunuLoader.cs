8f919a866755838a1f727cae14561abb22077a0c
azarashi2931 <kousuke.urakawa@gmail.com>
2019-03-29 05:42:10 +0900

    Update PauseMunuLoader.cs
    多重にメニューを開けないようにし、Escapeでメニューを閉じられるようにした

--- Assets/Scripts/CerkeOnline/Presentation/PauseMunuLoader.cs
+++ Assets/Scripts/CerkeOnline/Presentation/PauseMunuLoader.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UniRx;
 using Azarashi.Utilities;
@@ -9,14 +10,17 @@ namespace Azarashi.CerkeOnline.Presentation
 {
     [RequireComponent(typeof(IInputEventProvider))]
     public class PauseMunuLoader : MonoBehaviour
-    {   
+    {
         void Start()
         {
             //本当はこっちでやりたいけどこれじゃできない
             //var sceneLoader = new SceneAsyncLoader(SceneName.MainSceneUI.PauseMenu, LoadSceneMode.Additive);
             //GetComponent<IInputEventProvider>().OnPauseButton.TakeUntilDestroy(this).Subscribe(_ => sceneLoader.ChangeScene());
-            GetComponent<IInputEventProvider>().OnPauseButton.TakeUntilDestroy(this)
-                .Subscribe(_ => SceneManager.LoadScene(SceneName.MainSceneUI.PauseMenu, LoadSceneMode.Additive));
+            IObservable<Unit> onPauseButton = GetComponent<IInputEventProvider>().OnPauseButton.TakeUntilDestroy(this);
+            onPauseButton.Where(_ => !IsMenuLoaded()).Subscribe(_ => SceneManager.LoadScene(SceneName.MainSceneUI.PauseMenu, LoadSceneMode.Additive));
+            onPauseButton.Where(_ => IsMenuLoaded()).Subscribe(_ => SceneManager.UnloadSceneAsync(SceneName.MainSceneUI.PauseMenu));
         }
+
+        bool IsMenuLoaded() => SceneManager.GetSceneByName(SceneName.MainSceneUI.PauseMenu) != default;
     }
 }
