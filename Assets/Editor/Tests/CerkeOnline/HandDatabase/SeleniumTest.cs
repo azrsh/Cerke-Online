b88538fb9be8e2e7108e2e7c29dee71091dc77de
azarashi2931 <kousuke.urakawa@gmail.com>
2019-12-27 03:38:45 +0900

    役判定のテストのためにsleniumをインストール

---
+++ Assets/Editor/Tests/CerkeOnline/HandDatabase/SeleniumTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests
+{
+    public class SeleniumTest
+    {
+        [Test]
+        public void SeleniumTestSimplePasses()
+        {
+            var path = Application.streamingAssetsPath;
+            var options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            using (ChromeDriver driver = new ChromeDriver(path, options))
+            {
+                driver.Navigate().GoToUrl("https://yahoo.co.jp");
+                driver.FindElementByXPath("//*[@id=\"tabTopics2\"]/a").Click();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
