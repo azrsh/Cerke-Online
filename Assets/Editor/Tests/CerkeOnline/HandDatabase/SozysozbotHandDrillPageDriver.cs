62d74127ffa1d8453650ef72f4290a0ab49b252c
azarashi2931 <kousuke.urakawa@gmail.com>
2019-12-28 02:21:08 +0900

    役判定の照合関係のクラスを分割

---
+++ Assets/Editor/Tests/CerkeOnline/HandDatabase/SozysozbotHandDrillPageDriver.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using UnityEngine;
+using Azarashi.CerkeOnline.Domain.Entities;
+using Azarashi.CerkeOnline.Domain.Entities.Official.Pieces;
+
+namespace Azarashi.CerkeOnline.Tests
+{
+    public class SozysozbotHandDrillPageDriver
+    {
+        const string url = "https://sozysozbot.github.io/cerke_calculate_hands/calculate_hand_contest.html";
+
+        readonly RemoteWebDriver driver;
+
+        public SozysozbotHandDrillPageDriver(RemoteWebDriver driver)
+        {
+            this.driver = driver;
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void StartDrill(int drillCount)
+        {
+            IWebElement contestElement = driver.FindElementById("contest");
+            contestElement.FindElements(By.TagName("input"))
+                .Where(button => button.GetAttribute("value") == drillCount.ToString() + "本ノックを始める").First().Click();
+        }
+
+        public IReadOnlyPiece[] LoadQuestion()
+        {
+            string questionText = GetQuestionText(driver);
+            string[] questionElementsText = SplitQuestionText(questionText);
+            return ParseQuestionTextArrayToPieceArray(questionElementsText);
+        }
+
+        string GetQuestionText(RemoteWebDriver driver) => driver.FindElementById("contest").Text.Split('\n')[2];
+
+        string[] SplitQuestionText(string questionText) => questionText.Split(new string[] { "、" }, StringSplitOptions.None);
+
+        IReadOnlyPiece[] ParseQuestionTextArrayToPieceArray(string[] questionElementsText) =>
+            questionElementsText.Select<string, (string color, string kind)>(text => (text[0].ToString(), text[1].ToString()))
+                .Select(pair => (IReadOnlyPiece)ConvertPieceNameToPieceInstance(ConvertColorTextToColorId(pair.color), pair.kind)).ToArray();
+
+        public void PostAnswers(IHand[] answers)
+        {
+            IReadOnlyCollection<IWebElement> labelElements = driver.FindElementsByTagName("label");
+            foreach (IHand hand in answers)
+            {
+                string elementTextStyleHandName = TranslateHandNameToElementTextStyle(hand.Name);
+                IWebElement handTextElement = labelElements.Where(element => element.Text == elementTextStyleHandName).FirstOrDefault();
+
+                if (handTextElement == null) { Debug.LogError("webElement is null! : " + hand.Name); continue; }
+
+                string radioButtoElementId = handTextElement.GetAttribute("for");
+                driver.FindElementById(radioButtoElementId).Click();
+            }
+        }
+
+        public void DecideAnswer()
+        {
+            IWebElement decisionButton = driver.FindElementsByTagName("input").Where(element => element.GetAttribute("type") == "button")
+                .Where(element => element.GetAttribute("value") == "決定").FirstOrDefault();
+            decisionButton.Click();
+        }
+
+        //Pageじゃない方がいいかも（表示してるhtmlファイルは同じなので）
+        public void LoadNextPage()
+        {
+            IWebElement okButton = driver.FindElementsByTagName("input").Where(element => element.GetAttribute("type") == "button")
+                  .Where(element => element.GetAttribute("value") == "OK").FirstOrDefault();
+            okButton.Click();
+        }
+
+        public bool IsCorrect() => driver.FindElementByXPath("//*[@id=\"result\"]/strong").Text == "正解!";
+
+        public void ShowErrorMessage(IReadOnlyPiece[] questionPieces) =>
+            Debug.LogError("不正解\n" + questionPieces.Select(piece => piece.PieceName.ToString()) + "\n");
+
+        Terminologies.PieceColor ConvertColorTextToColorId(string colorText)
+        {
+            switch (colorText)
+            {
+                case "黒":
+                    return Terminologies.PieceColor.Black;
+                case "赤":
+                    return Terminologies.PieceColor.Red;
+                default:
+                    throw new ArgumentException("ColorText");
+            }
+        }
+
+        IPiece ConvertPieceNameToPieceInstance(Terminologies.PieceColor color, string pieceName)
+        {
+            switch (pieceName)
+            {
+                case Terminologies.Pieces.Felkana:
+                    return new Felkana(color, default, default, default);
+                case Terminologies.Pieces.Elmer:
+                    return new Elmer(color, default, default, default);
+                case Terminologies.Pieces.Gustuer:
+                    return new Gustuer(color, default, default, default);
+                case Terminologies.Pieces.Vadyrd:
+                    return new Vadyrd(color, default, default, default);
+                case Terminologies.Pieces.Stistyst:
+                    return new Stistyst(color, default, default, default);
+                case Terminologies.Pieces.Dodor:
+                    return new Dodor(color, default, default, default);
+                case Terminologies.Pieces.Kua:
+                    return new Kua(color, default, default, default);
+                case Terminologies.Pieces.Terlsk:
+                    return new Terlsk(color, default, default, default);
+                case Terminologies.Pieces.Varxle:
+                    return new Varxle(color, default, default, default);
+                case Terminologies.Pieces.Ales:
+                    return new Ales(color, default, default, default);
+                case Terminologies.Pieces.Tam:
+                    return new Tam(color, default, default, default);
+                default:
+                    throw new ArgumentException("piece name");
+            }
+        }
+
+        string TranslateHandNameToElementTextStyle(string handName)
+        {
+            if (handName == "王" || handName == "同色王")
+                return "王 = 同色王";
+
+            return handName;
+        }
+    }
+}
--- Assets/Editor/Tests/CerkeOnline/HandDatabase/SozysozbotHandQuestionEngine.cs
+++ Assets/Editor/Tests/CerkeOnline/HandDatabase/SozysozbotHandQuestionEngine.cs
@@ -10,13 +10,12 @@ using Azarashi.CerkeOnline.Domain.Entities.Official.Pieces;
 
 namespace Azarashi.CerkeOnline.Tests
 {
+
     public class SozysozbotHandQuestionEngine
     {
-        const string url = "https://sozysozbot.github.io/cerke_calculate_hands/calculate_hand_contest.html";
-        
         readonly Func<IReadOnlyPiece[], IHand[]> solveFunction;
         readonly int drillCount;
-
+        
         public SozysozbotHandQuestionEngine(Func<IReadOnlyPiece[], IHand[]> solveFunction, int drillCount = 50)
         {
             this.solveFunction = solveFunction;
@@ -30,96 +29,28 @@ namespace Azarashi.CerkeOnline.Tests
             options.AddArgument("--headless");
             using (ChromeDriver driver = new ChromeDriver(path, options))
             {
-                driver.Navigate().GoToUrl(url);
-                IWebElement contestElement = driver.FindElementById("contest");
-                contestElement.FindElements(By.TagName("input"))
-                    .Where(button => button.GetAttribute("value") == drillCount.ToString() + "本ノックを始める").First().Click();
+                SozysozbotHandDrillPageDriver drillPageDriver = new SozysozbotHandDrillPageDriver(driver);
+               
+                drillPageDriver.StartDrill(drillCount);
 
                 for (int i = 0; i < drillCount; i++)
-                    SolveQuestion(driver);
+                { 
+                    VerifyAnswerToQuestion(drillPageDriver); 
+                    drillPageDriver.LoadNextPage(); 
+                }
 
-                System.Threading.Thread.Sleep(5000);
+                //System.Threading.Thread.Sleep(10000);
             }
         }
 
-        void SolveQuestion(RemoteWebDriver driver)
+        void VerifyAnswerToQuestion(SozysozbotHandDrillPageDriver drillPageDriver)
         {
-            string questionText = driver.FindElementById("contest").Text.Split('\n')[2];
-            string[] questionElementsText = questionText.Split(new string[] { "、" }, StringSplitOptions.None);
-            IReadOnlyPiece[] questionPieces = questionElementsText.Select<string, (string color, string kind)>(text => (text[0].ToString(), text[1].ToString()))
-                .Select(pair => (IReadOnlyPiece)ConvertPieceNameToPieceInstance(ConvertColorTextToColorId(pair.color), pair.kind)).ToArray();
-
+            IReadOnlyPiece[] questionPieces = drillPageDriver.LoadQuestion();
             IHand[] hands = solveFunction(questionPieces);
-            IReadOnlyCollection<IWebElement> labelElements = driver.FindElementsByTagName("label");
-            foreach (IHand hand in hands)
-            {
-                IWebElement handTextElement;
-                if (hand.Name == "王" || hand.Name == "同色王")
-                    handTextElement = labelElements.Where(element => element.Text == "王 = 同色王").FirstOrDefault();
-                else
-                    handTextElement = labelElements.Where(element => element.Text == hand.Name).FirstOrDefault();
+            drillPageDriver.PostAnswers(hands);
+            drillPageDriver.DecideAnswer();
 
-                if (handTextElement == null) { Debug.LogError("webElement is null! : " + hand.Name); continue; }
-
-                string radioButtoElementId = handTextElement.GetAttribute("for");
-                driver.FindElementById(radioButtoElementId).Click();
-            }
-
-            IWebElement decisionButton = driver.FindElementsByTagName("input").Where(element => element.GetAttribute("type") == "button")
-                .Where(element => element.GetAttribute("value") == "決定").FirstOrDefault();
-            decisionButton.Click();
-
-            bool isTrue = driver.FindElementByXPath("//*[@id=\"result\"]/strong").Text == "正解!";
-            if (!isTrue) { Debug.LogError("不正解\n" + questionText + "\n"); }
-
-            IWebElement okButton = driver.FindElementsByTagName("input").Where(element => element.GetAttribute("type") == "button")
-                .Where(element => element.GetAttribute("value") == "OK").FirstOrDefault();
-            okButton.Click();
-        }
-
-        int ConvertColorTextToColorId(string colorText)
-        {
-            switch (colorText)
-            {
-                case "黒":
-                    return 0;
-                case "赤":
-                    return 1;
-                default:
-                    throw new ArgumentException("ColorText");
-            }
-        }
-
-        IPiece ConvertPieceNameToPieceInstance(int colorId, string pieceName)
-        {
-            switch (pieceName)
-            {
-                case Terminologies.Pieces.Felkana:
-                    return new Felkana(colorId, default, default, default);
-                case Terminologies.Pieces.Elmer:
-                    return new Elmer(colorId, default, default, default);
-                case Terminologies.Pieces.Gustuer:
-                    return new Gustuer(colorId, default, default, default);
-                case Terminologies.Pieces.Vadyrd:
-                    return new Vadyrd(colorId, default, default, default);
-                case Terminologies.Pieces.Stistyst:
-                    return new Stistyst(colorId, default, default, default);
-                case Terminologies.Pieces.Dodor:
-                    return new Dodor(colorId, default, default, default);
-                case Terminologies.Pieces.Kua:
-                    return new Kua(colorId, default, default, default);
-                case Terminologies.Pieces.Terlsk:
-                    return new Terlsk(colorId, default, default, default);
-                case Terminologies.Pieces.Varxle:
-                    return new Varxle(colorId, default, default, default);
-                case Terminologies.Pieces.Ales:
-                    return new Ales(colorId, default, default, default);
-                case Terminologies.Pieces.Tam:
-                    return new Tam(colorId, default, default, default);
-                default:
-                    throw new ArgumentException("piece name");
-            }
-        
+            if (!drillPageDriver.IsCorrect()) drillPageDriver.ShowErrorMessage(questionPieces);
         }
     }
 }
