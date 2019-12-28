007941572bcc56e5a5d7ba603461bd79403c6d7d
azarashi2931 <kousuke.urakawa@gmail.com>
2019-12-29 00:07:06 +0900

    MovePredicterの名前空間とフォルダを再配置

---
+++ Assets/Scripts/CerkeOnline/Domain/Entities/MovePredictor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Azarashi.CerkeOnline.Domain.Entities
+{
+    public class MovePredictor
+    {
+        readonly IBoard board;
+
+        public MovePredictor(IBoard board)
+        {
+            this.board = board;
+        }
+
+        public IReadOnlyList<Vector2Int> PredictMoveableColumns(IReadOnlyPiece movingPiece)
+        {
+            List<Vector2Int> result = new List<Vector2Int>();
+
+            if (movingPiece == null) return result;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Vector2Int currentPosition = new Vector2Int(i, j);
+                    if (board.IsOnBoard(currentPosition) && movingPiece.IsMoveable(currentPosition))
+                        result.Add(currentPosition);
+                }
+            }
+
+            return result;
+        }
+    }
+}
