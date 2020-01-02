3321ec0ea5cbdef248356ca58f20b8a543d86e10
azarashi2931 <kousuke.urakawa@gmail.com>
2020-01-02 23:31:43 +0900

    不要なスクリプトを削除

--- Assets/Scripts/CerkeOnline/Presentation/Presenter/BoardPresenter.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Azarashi.CerkeOnline.Presentation.Presenter
-{
-    /// <summary>
-    /// Unused. 責務が存在しない. 削除を検討.
-    /// </summary>
-    public class BoardPresenter : MonoBehaviour { }
-}
