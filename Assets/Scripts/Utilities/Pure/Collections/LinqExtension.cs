eee0576ca88eb1081db7e11840ce0387af740895
azarashi2931 <kousuke.urakawa@gmail.com>
2020-03-30 00:42:00 +0900

    シーケンスの順序無視の一致検査のための拡張メソッドを追加

---
+++ Assets/Scripts/Utilities/Pure/Collections/LinqExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Azarashi.Utilities.Collections
+{
+    public static class LinqExtension
+    {
+        public static bool SequenceMatch<T>(this IEnumerable<T> enumerable1, IEnumerable<T> enumerable2)
+            => !enumerable1.Except(enumerable2).Any() && !enumerable2.Except(enumerable1).Any();
+    }
+}
