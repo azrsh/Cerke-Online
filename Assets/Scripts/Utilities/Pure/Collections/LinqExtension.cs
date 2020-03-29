81150414f37a30fc3c841745f09efe8748511736
azarashi2931 <kousuke.urakawa@gmail.com>
2020-03-30 02:35:46 +0900

    注釈の追加

--- Assets/Scripts/Utilities/Pure/Collections/LinqExtension.cs
+++ Assets/Scripts/Utilities/Pure/Collections/LinqExtension.cs
@@ -5,6 +5,13 @@ namespace Azarashi.Utilities.Collections
 {
     public static class LinqExtension
     {
+        /// <summary>
+        /// 重複のないIEnumerable<T>の要素が一致するか確認します. 重複がある場合は, 含まれる要素の種類が一致していればtrueを返却します.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable1"></param>
+        /// <param name="enumerable2"></param>
+        /// <returns></returns>
         public static bool SequenceMatch<T>(this IEnumerable<T> enumerable1, IEnumerable<T> enumerable2)
             => !enumerable1.Except(enumerable2).Any() && !enumerable2.Except(enumerable1).Any();
     }
