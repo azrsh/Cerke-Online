c91617e53c9b64f88b21db0dd14782f972b440c4
azarashi2931 <kousuke.urakawa@gmail.com>
2020-03-30 03:43:06 +0900

    HandChangeObserverのバグを修正

--- Assets/Scripts/CerkeOnline/Domain/Entities/HandChangeObserver.cs
+++ Assets/Scripts/CerkeOnline/Domain/Entities/HandChangeObserver.cs
@@ -12,21 +12,29 @@ namespace Azarashi.CerkeOnline.Domain.Entities
         internal IObservable<IReadOnlyPlayer> Observable => subject;
         Subject<IReadOnlyPlayer> subject = new Subject<IReadOnlyPlayer>();
 
+        //前回の役の保存、これでいいのか？
+        IDictionary<IReadOnlyPlayer, IEnumerable<IHand>> previousHandsDictionary = new Dictionary<IReadOnlyPlayer, IEnumerable<IHand>>();
+
         internal HandChangeObserver(IHandDatabase handDatabase, IObservable<IReadOnlyPlayer> onTurnEnd)
         {
             this.handDatabase = handDatabase;
             onTurnEnd.Where(CheckHandIncrease).Subscribe(subject);
         }
 
-        //前回の役の保存、これでいいのか？
-        IHand[] previousHands = new IHand[0];
         bool CheckHandIncrease(IReadOnlyPlayer currentPlayer)
         {
+            if(!previousHandsDictionary.ContainsKey(currentPlayer))
+                previousHandsDictionary.Add(currentPlayer, Enumerable.Empty<IHand>());
+
             IEnumerable<IHand> currentHands = handDatabase.SearchHands(currentPlayer.GetPieceList());
-            HandDifference handDifference = HandDifferenceCalculator.Calculate(previousHands, currentHands);
-            return handDifference.IncreasedDifference.Count() > 0;
+            HandDifference handDifference = HandDifferenceCalculator.Calculate(previousHandsDictionary[currentPlayer], currentHands);
+            
+            if(handDifference.IncreasedDifference.Any() || handDifference.IncreasedDifference.Any())
+                previousHandsDictionary[currentPlayer] = currentHands.ToArray();
+            
+            return handDifference.IncreasedDifference.Any();
         }
 
-        internal void Reset() => previousHands = new IHand[0];
+        internal void Reset() => previousHandsDictionary.Clear();
     }
 }
