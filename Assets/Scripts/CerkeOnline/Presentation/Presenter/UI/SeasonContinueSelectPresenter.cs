8eb23b80f108229d9af799c45ef121d35d5c0024
azarashi2931 <kousuke.urakawa@gmail.com>
2020-04-04 03:11:33 +0900

    #24 終季/再行の選択ログに翻訳を適用

--- Assets/Scripts/CerkeOnline/Presentation/Presenter/UI/SeasonContinueSelectPresenter.cs
+++ Assets/Scripts/CerkeOnline/Presentation/Presenter/UI/SeasonContinueSelectPresenter.cs
@@ -3,6 +3,7 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Azarashi.CerkeOnline.Application;
+using Azarashi.CerkeOnline.Application.Language;
 using Azarashi.CerkeOnline.Domain.Entities;
 
 namespace Azarashi.CerkeOnline.Presentation.Presenter.UI
@@ -35,7 +36,9 @@ namespace Azarashi.CerkeOnline.Presentation.Presenter.UI
             Action<SeasonContinueOrEnd> action = continueOrEnd =>
             {
                 Close();
-                GameController.Instance.SystemLogger.Log(continueOrEnd);
+                TranslatableKeys key = continueOrEnd == SeasonContinueOrEnd.Continue ? TranslatableKeys.DeclaringToContinueButton : TranslatableKeys.DeclaringToEndButton;
+                var message = LanguageManager.Instance.Translator.Translate(key);
+                GameController.Instance.SystemLogger.Log(message);
                 callback(continueOrEnd);
                 IsRequestCompleted = true;
             };
